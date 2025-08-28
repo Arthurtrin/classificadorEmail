7a515e04b51c0247d7a507a23518c747dec9fa83
Arthurtrin <arthurtrindade08102004@gmail.com>
2025-08-28 15:31:35 -0300

    primeiro prototipo do projeto pronto

--- main/views.py
+++ main/views.py
@@ -7,6 +7,11 @@ from nltk.corpus import stopwords
 import nltk
 from unidecode import unidecode
 from django.contrib import messages
+from django.conf import settings
+import openai
+
+# Pega a chave diretamente do settings
+openai.api_key = settings.OPENAI_API_KEY
 
 def extrairConteudoArquivo(arquivo):
     if arquivo.name.endswith(".txt"):
@@ -41,28 +46,67 @@ def pegaremail(request):
         if request.FILES.get("arquivo") and request.POST.get('emailText'):
             messages.error(request, 'Decida o formato a ser submetido (Arquivo ou Texto).')
             return redirect('pegaremail')
+        
         elif request.POST.get('emailText'):
             conteudo = request.POST.get('emailText')
+
         elif request.FILES.get("arquivo"):
             arquivo = request.FILES["arquivo"]
 
             if not validaArquivo(arquivo):
                 messages.error(request, 'Formato de arquivo não suportado, apenas PDF ou txt.')
                 return redirect('pegaremail')
-            
             conteudo = extrairConteudoArquivo(arquivo) # EXTRAIR O CONTEUDO DO ARQUIVO
-            textoPrePro = preprocessar(conteudo) # PREPROCESSAR O TEXTO (SENDO TEXTO OU PDF/TXT)
-
+        
         else:
             messages.error(request, 'Envie o email por texto ou por arquivo')
             return redirect('pegaremail')
+        
+        textoPrePro = preprocessar(conteudo) # PREPROCESSAR O TEXTO (SENDO TEXTO OU PDF/TXT)
 
-        categoria = ""
-        resposta = ""
-        dados = {
-            "categoria": categoria,
-            "respSugerida": resposta
-            }
+        textoResposta  = classificaEmail(textoPrePro)
+        print(textoResposta)
+        dados = separaTexto(textoResposta)
+
         return render(request, 'main/index.html', dados)
     return render(request, 'main/index.html')
 
+
+def separaTexto(texto_resposta):
+    categoria = None
+    resposta = None
+
+    # Captura a categoria
+    match_categoria = re.search(r"Classificação:\s*\*\*(.*?)\*\*", texto_resposta)
+    if match_categoria:
+        categoria = match_categoria.group(1).strip()
+
+    # Captura a resposta automática
+    match_resposta = re.search(r"Resposta automática:\s*\"(.*)\"", texto_resposta, re.DOTALL)
+    if match_resposta:
+        resposta = match_resposta.group(1).strip()
+
+    return {
+            "categoria": categoria,
+            "resposta": resposta
+            }
+
+
+def classificaEmail(email_texto):
+    prompt = f"""
+    Analise o seguinte email e:
+    1. Classifique como 'Produtivo' ou 'Improdutivo'.
+    2. Gere uma resposta automática curta e adequada.
+
+    Email: {email_texto}
+    Sempre retorne no formato:
+    'Classificação: **[Produtivo/Improdutivo]**
+    Resposta automática: "[resposta gerada]"'
+    """
+    
+    resposta = openai.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    
+    return resposta.choices[0].message.content
--- smartEmail/settings.py
+++ smartEmail/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e2%-b-s^n72_+ir1a%+clia*2(dmui^mt!bd_26g8i#qmm(tk%'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = config("SECRET_KEY")
+DEBUG = config("DEBUG", default=False, cast=bool)
+OPENAI_API_KEY = config("OPENAI_API_KEY")
 
 ALLOWED_HOSTS = []
 
@@ -122,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
